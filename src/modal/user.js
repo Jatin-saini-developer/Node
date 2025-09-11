ffe1084b179620f25227ddd02d536974c3f3e5e4
Jatin Saini <jatinrohitsaini@gmail.com>
2025-09-11 18:09:29 +0530

    Update Schema

--- src/modal/user.js
+++ src/modal/user.js
@@ -1,26 +1,39 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    firstName:{
-        type: String,
-        required: true
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 4,
+      maxlength: 30,
+      trim: true,
     },
-    lastName:{
-        type:String,
-        required:true
+    lastName: {
+      type: String,
+      required: true,
     },
-    email : {
-        type : String,
-        required : true,
-        unique : true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    password : {
-        type : String
+    password: {
+      type: String,
     },
-    age : {
-        type : String
-    }
-});
+    age: {
+      type: Number,
+      min: 18,
+    },
+    about: {
+      type: String,
+      default: "i am in Tech",
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
