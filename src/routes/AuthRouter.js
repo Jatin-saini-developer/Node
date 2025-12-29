945f74b16d62b85e3474a14c1084b05fdb47b6fc
Jatin Saini <jatinrohitsaini@gmail.com>
2025-12-29 13:20:02 +0530

    Build SignUp Feature

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -4,16 +4,11 @@ const bcrypt = require("bcrypt");
 const User = require("../modal/user");
 const jwt = require("jsonwebtoken");
 
-
-
-
-
 const AuthRouter = express.Router();
 
 AuthRouter.post("/signUp", async (req, res) => {
   try {
-     validateSignUpData(req);
-
+    validateSignUpData(req);
 
     const { firstName, lastName, email, password } = req.body;
 
@@ -27,7 +22,17 @@ AuthRouter.post("/signUp", async (req, res) => {
       email,
     });
 
+    const token = jwt.sign({ _id: obj._id }, "DEV@TINDER69", {
+      expiresIn: "1d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      sameSite: "lax", 
+    });
+
     await obj.save();
+
+
     res.send("User signed up successfully");
   } catch (err) {
     console.error("Signup error:", err.message);
@@ -35,42 +40,38 @@ AuthRouter.post("/signUp", async (req, res) => {
   }
 });
 
-AuthRouter.post("/logout", async(req, res)=>{
-    res.cookie("token", null, {
-        expires : new Date(Date.now())
-    });
+AuthRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
 
-    res.send("Logged Out Sucessfully");
+  res.send("Logged Out Sucessfully");
 });
 
-AuthRouter.post("/login", async(req, res) =>{
-  try{
-    const {email, password} = req.body;
+AuthRouter.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-    const user = await User.findOne({email : email});
-    if(!user){
+    const user = await User.findOne({ email: email });
+    if (!user) {
       throw new Error("User is not Signed Up, Please Sign Up ");
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if(isPasswordValid){
+    if (isPasswordValid) {
+      const token = jwt.sign({ _id: user._id }, "DEV@TINDER69", {
+        expiresIn: "1d",
+      });
 
-      const token = jwt.sign({_id : user._id}, "DEV@TINDER69",{expiresIn : "1d"})
-
-
-
-      res.cookie("token", token)
+      res.cookie("token", token);
       res.send(user);
-
-    }else{
+    } else {
       throw new Error("Login Error, Please try again or signUp first");
     }
-    
-  }catch(err){
-    res.status(404).send(err.message)
-
+  } catch (err) {
+    res.status(404).send(err.message);
   }
-})
+});
 
 module.exports = AuthRouter;
